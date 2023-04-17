4fd548cdb164edfcf00f3918eb85776af44f20f0
XanderVerMac <vera.macias26.av@gmail.com>
2023-04-17 11:25:31 -0500

    se hicieron los tres primeros botones de la calculadora.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
-import tkinter
+from tkinter import *
 
-root=tkinter.Tk()
+root=Tk()
 root.title("calculator")
 
-display=tkinter.Entry(root)
-display.grid(row=0,columnspan=5)
+display=Entry(root)
+display.grid(row=0,columnspan=5,sticky=W+E)
+Button(root,text="1").grid(row=1,column=0,sticky=W+E)
+Button(root,text="2").grid(row=1,column=1,sticky=W+E)
+Button(root,text="3").grid(row=1,column=2,sticky=W+E)
 root.mainloop()
